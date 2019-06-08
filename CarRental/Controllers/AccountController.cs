316336eb4af0c26d7d4151043b51a0aec92d2202
Farlier <v.zamikula@student.khai.edu>
2019-06-08 19:30:34 +0300

    Add project files.

---
+++ CarRental/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using Models;
+using Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace CarRental.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        public ActionResult Login()
+        {
+            return PartialView(new ViewModelUserLogin());
+        }
+
+        [HttpPost]
+        public ActionResult Login(ViewModelUserLogin user)
+        {
+            return PartialView();
+        }
+
+        public ActionResult Register()
+        {
+            return View(new ViewModelUserRegister());
+        }
+
+        [HttpPost]
+        public ActionResult Register(ViewModelUserRegister newUser)
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Models/ViewModels/ViewModelUserRegister.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.ViewModels
+{
+    public class ViewModelUserRegister
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string PasswordRepeat { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public decimal Phone { get; set; }
+    }
+}
